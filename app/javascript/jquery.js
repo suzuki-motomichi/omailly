6ab78193bd20d578c87c0dcf0cb00edc99c24763
suzuki-motomichi <cw.sm31@gmail.com>
2021-08-14 11:54:20 +0900

    Fix: モーダル修正

--- app/javascript/jquery.js
+++ app/javascript/jquery.js
@@ -1,3 +1,63 @@
+let str = [
+  "思い出してみてください<br>",
+  "輝いていたあの頃を<br>",
+  "たくさんの方に使ってもらえた日々を<br>",
+  "そもそも使ってもらえなかったかもしれません<br>",
+  "思い出してくれて<br>",
+  "ありがとう",
+  "さようなら",
+];
+
+$(() => {
+  $(".js-open").click(() => {
+    $(document).ready(() => {
+      for (let i = 0; i < str.length; i++) {
+        $("#output").append("<div>" + str[i] + "</div>");
+      }
+      $("#output div").each(function (i) {
+        $(this)
+          .hide()
+          .delay(2000 * i)
+          .fadeIn(3000);
+      });
+    });
+    $("#overlay, .modal-window").fadeIn();
+  });
+});
+
+$(() => {
+  $(".js-close")
+    .unbind()
+    .click(() => {
+      $("#overlay, .modal-window").fadeOut();
+      $("#output div").remove();
+    });
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  let section = document.querySelector(".cherry-blossom-container");
+
+  let createPetal = () => {
+    let petalEl = document.createElement("span");
+    petalEl.className = "petal";
+    let minSize = 10;
+    let maxSize = 17;
+    let size = Math.random() * (maxSize + 1 - minSize) + minSize;
+    petalEl.style.width = `${size}px`;
+    petalEl.style.height = `${size}px`;
+    petalEl.style.left = Math.random() * innerWidth + "px";
+    section.appendChild(petalEl);
+
+    setTimeout(() => {
+      petalEl.remove();
+    }, 10000);
+  };
+
+  setInterval(createPetal, 120);
+});
+
+
+
 // const { validate } = require("webpack");
 // $(function(){
 //   function setWord(){
@@ -8,75 +68,75 @@
 
 // debugger
 
-$(function () {
-  $('.js-open').click(function () {
-    // if (modalOn === true) {
-    //   return;
-    // }
-    // modalOn = true;
-    // console.log('foo');
-    $(document).ready(function(){
-      setTimeout(function(){
-        $('.output.1').hide().append(str01).fadeIn(1500);
-      }, 1500);
-      setTimeout(function(){
-        $('.output.2').hide().append(str02).fadeIn(2000);
-      }, 4500);
-      setTimeout(function(){
-        $('.output.3').hide().append(str03).fadeIn(2000);
-      }, 7500);
-      setTimeout(function(){
-        $('.output.4').hide().append(str04).fadeIn(1500);
-      }, 10000);
-      setTimeout(function(){
-        $('.output.5').hide().append(str05).fadeIn(1500);
-      }, 13000);
-      setTimeout(function(){
-        $('.output.6').hide().append(str06).fadeIn(1500);
-      }, 16000);
-      setTimeout(function(){
-        $('.output.7').hide().append(str07).fadeIn(3500);
-      }, 19000);
-      setTimeout(function(){
-        $('.twitter-share').hide().fadeIn(3600);
-      }, 20000);
-      })
-  // if($( "#overlay")[1]) return true;
-    $('#overlay, .modal-window').fadeIn();
-  });
-});
-// debugger
-$(function () {
-$('.js-close').unbind().click(function () {
-  // if (modalOff === true) {
-  //   return;
-  // }
-  // if (modalOn === false) {
-    // $(".output").css("display", "none");
+// $(function () {
+//   $('.js-open').click(function () {
+//     // if (modalOn === true) {
+//     //   return;
+//     // }
+//     // modalOn = true;
+//     // console.log('foo');
+//     $(document).ready(function(){
+//       setTimeout(function(){
+//         $('.output.1').hide().append(str01).fadeIn(1500);
+//       }, 1500);
+//       setTimeout(function(){
+//         $('.output.2').hide().append(str02).fadeIn(2000);
+//       }, 4500);
+//       setTimeout(function(){
+//         $('.output.3').hide().append(str03).fadeIn(2000);
+//       }, 7500);
+//       setTimeout(function(){
+//         $('.output.4').hide().append(str04).fadeIn(1500);
+//       }, 10000);
+//       setTimeout(function(){
+//         $('.output.5').hide().append(str05).fadeIn(1500);
+//       }, 13000);
+//       setTimeout(function(){
+//         $('.output.6').hide().append(str06).fadeIn(1500);
+//       }, 16000);
+//       setTimeout(function(){
+//         $('.output.7').hide().append(str07).fadeIn(3500);
+//       }, 19000);
+//       setTimeout(function(){
+//         $('.twitter-share').hide().fadeIn(3600);
+//       }, 20000);
+//       })
+//   // if($( "#overlay")[1]) return true;
+//     $('#overlay, .modal-window').fadeIn();
+//   });
+// });
+// // debugger
+// $(function () {
+// $('.js-close').unbind().click(function () {
+//   // if (modalOff === true) {
+//   //   return;
+//   // }
+//   // if (modalOn === false) {
+//     // $(".output").css("display", "none");
 
-  // }
-  // $('.output').remove();
-  // modalOff = true;
-  // 無理やり更新するボタン
-  // window.location.reload();
-  $('#overlay, .modal-window').fadeOut()
-    // function(){
-    $('.output').remove();
-    // $(".output").css("display", "none");
-    // });
-  });
-});
+//   // }
+//   // $('.output').remove();
+//   // modalOff = true;
+//   // 無理やり更新するボタン
+//   // window.location.reload();
+//   $('#overlay, .modal-window').fadeOut()
+//     // function(){
+//     $('.output').remove();
+//     // $(".output").css("display", "none");
+//     // });
+//   });
+// });
 
-let str01 = '思い出してみてください<br>';
-let str02 = '輝いていたあの頃を<br>';
-let str03 = 'たくさんの方に使ってもらえた日々を<br>';
-let str04 = 'そもそも使ってもらえなかったかもしれません<br>';
-let str05 = '思い出してくれて<br>';
-let str06 = 'ありがとう';
-let str07 = 'さようなら';
+// let str01 = '思い出してみてください<br>';
+// let str02 = '輝いていたあの頃を<br>';
+// let str03 = 'たくさんの方に使ってもらえた日々を<br>';
+// let str04 = 'そもそも使ってもらえなかったかもしれません<br>';
+// let str05 = '思い出してくれて<br>';
+// let str06 = 'ありがとう';
+// let str07 = 'さようなら';
 
-let modalOn = false;
-let modalOff = false;
+// let modalOn = false;
+// let modalOff = false;
 
 console.log('ほげ');
 
