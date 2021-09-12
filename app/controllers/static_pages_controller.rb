3f5e1be176b72fb1f0b0bce3b495e1b078897da7
suzuki-motomichi <cw.sm31@gmail.com>
2021-09-13 08:30:17 +0900

    add: 使い方ページ作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,6 @@ class StaticPagesController < ApplicationController
   def terms; end
 
   def privacy; end
+
+  def description; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#top'
   get '/terms', to: 'static_pages#terms'
   get '/privacy', to: 'static_pages#privacy'
+  get '/description', to: 'static_pages#description'
 
   resources :posts do
     post :confirm, action: :confirm_new, on: :new
