32b24975b0c06980d08d29eb742bcfbc3e8044d5
suzuki-motomichi <cw.sm31@gmail.com>
2021-09-12 13:49:34 +0900

    add: サーバーが起動しなかったバグを修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,11 +3,13 @@ const { environment } = require('@rails/webpacker')
 
 // ↓ 追記部分
 const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery'
-    })
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  })
 )
 
 module.exports = environment
