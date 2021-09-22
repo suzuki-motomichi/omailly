d536352e1b5b6f1a7e91d93f0a4d91cdec069527
suzuki-motomichi <cw.sm31@gmail.com>
2021-09-23 07:49:18 +0900

    add: 改行判定

--- app/javascript/font.js
+++ app/javascript/font.js
@@ -1,38 +1,22 @@
 $(document).ready(function(){
-  $(".post-title").each(function() {
-    var menu_len = $(this).html();
+    $(".post-title").each(function() {
+    let menu_len = $(".post-title p").text().replace(/\n/g, "");
     console.log(menu_len.length);
-    // switch(menu_len.length) {
-    //   case :
-    //   break;
-    // }
-
 
     if(1 < menu_len.length && menu_len.length <= 5){
-        $(this).addClass("small");
+        $(".post-title p").addClass("small");
     }else if(5 < menu_len.length && menu_len.length <= 10){
-        $(this).addClass("small-medium");
+        $(".post-title p").addClass("small-medium");
     }else if(10 < menu_len.length && menu_len.length <= 15){
-        $(this).addClass("medium");
+        $(".post-title p").addClass("medium");
     }else if(15 < menu_len.length && menu_len.length <= 20){
-        $(this).addClass("medium-large");
+        $(".post-title p").addClass("medium-large");
+    }else if(20 < menu_len.length && menu_len.length <= 25){
+        $(".post-title p").addClass("large");
     }else if(25 < menu_len.length && menu_len.length <= 30){
-        $(this).addClass("large");
-    }else if(30 < menu_len.length && menu_len.length <= 35){
-        // console.log(menu_len.length);
+        $(".post-title p").addClass("ex-large");
     }else{
-        $(this).addClass("ex-large");
-      };
+        $(".post-title p").addClass("ex-ex-large");
+        };
     });
-  });
-
-  // $(document).ready(function(){
-  //   $(".post-title").each(function() {
-  //     var menu_len = $(this).html();
-  //     if(menu_len.length > 20){
-  //       $(this).addClass("large");
-  //     }
-  //     console.log(menu_len.length);
-  //     });
-  //   });
-
+    });
---
+++ app/javascript/packs/return.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', function(){
+  const MAX_LINE_NUM = 3;
+  const textarea = document.getElementById("post_name");
+  textarea.addEventListener('input', function() {
+      console.log('どうです？');
+      let lines = textarea.value.split("\n");
+      if (lines.length > MAX_LINE_NUM) {
+      let result = "";
+      for (let i = 0; i < MAX_LINE_NUM; i++) {
+      result += lines[i] + "\n";
+      }
+      textarea.value = result;
+      }
+  });
+  }, false);
