78abb7219181299709c15565c09071ad3cd61f52
suzuki-motomichi <cw.sm31@gmail.com>
2021-09-23 07:50:25 +0900

    fix: validate変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 40 }
+  validates :title, presence: true, length: { maximum: 35 }
   validates :content, presence: true, length: { maximum: 400 }
   validates :last_day, length: { maximum: 9999 }
 
