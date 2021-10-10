720356b0bec8c133db99965e6a95c01a60c2e171
suzuki-motomichi <cw.sm31@gmail.com>
2021-10-10 19:39:29 +0900

    add: フォーム内改行

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   validates :title, check_person_name: true, presence: true, length: { maximum: 30 }
-  validates :content, presence: true, length: { maximum: 400 }
+  validates :content, presence: true, length: { maximum: 200 }
   validates :last_day, length: { maximum: 9999 }
 
   enum status: { years: 0, month: 1, day: 2, hours: 3, minute: 4, seconds: 5 }
