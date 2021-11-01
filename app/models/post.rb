02d8b4d81249caf5669fa2948c8efedb77753227
suzuki-motomichi <cw.sm31@gmail.com>
2021-11-01 23:20:15 +0900

    add:context追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   validates :title, check_person_name: true, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 200 }
-  validates :last_day, length: { maximum: 9999 }
+  validates :last_day, length: { maximum: 4 }, allow_blank: true, numericality: {only_integer: true}
 
   enum status: { years: 0, month: 1, day: 2, hours: 3, minute: 4, seconds: 5 }
 end
