a2dff24fc59360cc390a4fd15af27ebeb255ef5b
suzuki-motomichi <cw.sm31@gmail.com>
2022-01-26 01:33:30 +0900

    add: 外部キー追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_134705) do
+ActiveRecord::Schema.define(version: 2022_01_25_153825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body", null: false
+    t.bigint "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
@@ -23,6 +31,8 @@ ActiveRecord::Schema.define(version: 2022_01_24_134705) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status", default: 0
     t.integer "likes", default: 0
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
