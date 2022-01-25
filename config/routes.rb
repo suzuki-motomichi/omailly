8862082973e3d924165d52306b09c63d7464d553
suzuki-motomichi <cw.sm31@gmail.com>
2022-01-25 11:43:35 +0900

    add: コントローラ・ルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   get '/privacy', to: 'static_pages#privacy'
   get '/description', to: 'static_pages#description'
 
+  # ログイン
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: %i[new create], param: :uuid
   resources :posts do
     post :confirm, action: :confirm_new, on: :new
     post 'likes', to: 'posts#likes'
