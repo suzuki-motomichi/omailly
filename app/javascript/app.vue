9f4cbc5925e9830baac09ea61130da00b1cd9884
suzuki-motomichi <cw.sm31@gmail.com>
2022-02-08 13:50:07 +0900

    add: $ yarn add axios

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,20 +1,38 @@
 <template>
   <div id="app">
+    <div>
+      <Ranking />
+    </div>
     <p>{{ message }}</p>
   </div>
 </template>
 
 <script>
+import Ranking from "./components/Ranking";
+
+const router = new VueRouter({
+  routes: [
+    { path: '/',
+      component: Ranking  }
+  ]
+})
+
 export default {
+  components: {
+    Ranking
+  },
   data: function () {
     return {
       message: "Hello Vue!"
     }
   }
-}
+};
 </script>
 
 <style scoped>
+html {
+  background-color: rgba(5, 5, 5, 0.89);
+}
 p {
   font-size: 2em;
   text-align: center;
--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -7,6 +7,10 @@
 
 import Vue from 'vue'
 import App from '../app.vue'
+import BootstrapVue from 'bootstrap-vue'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
