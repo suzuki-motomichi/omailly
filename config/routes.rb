001074dbf9e645e5d8a0e7b36f96356e73dc3d71
suzuki-motomichi <cw.sm31@gmail.com>
2022-02-15 11:39:58 +0900

    add: ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
+  # SNSログイン
+  post 'oauth/callback', to: 'oauths#callback'
+  get 'oauth/callback', to: 'oauths#callback'
+  get 'oauth/:provider', to: 'oauths#oauth', as: :auth_at_provider
+
   resources :users, only: %i[new create], param: :uuid
   resources :posts do
     post :confirm, action: :confirm_new, on: :new
